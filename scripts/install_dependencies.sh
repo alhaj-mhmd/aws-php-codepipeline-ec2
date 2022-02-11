94a5fbac3cd577a3722dc0996683fc1ee58d2918
Mohammad <alhaj.mhmd@gmail.com>
2022-02-11 23:56:11 +0800

    edit before install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,6 @@
    #!/bin/bash
 
-    dpkg -s apache2 &> /dev/null  
+    sudo apt-get update
+    sudo apt-get install apache2
 
-    if [ $? -ne 0 ]
-
-        then
-            
-            sudo apt-get update
-            sudo apt-get install apache2
-
-        else
-            echo    "apach2 installed"
-    fi
+      
