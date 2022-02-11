3a4e31657dd7844c3a0462aabbec12bc0a09777a
Mohammad <alhaj.mhmd@gmail.com>
2022-02-12 00:59:10 +0800

    update project

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-cd /var/www/html
-curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
-HASH=`curl -sS https://composer.github.io/installer.sig`
-
-php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 
 
